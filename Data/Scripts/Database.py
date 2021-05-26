68a34c95137e13054c289c044f6301db71a92157
Maximus Germer <mg26kapo@studserv.uni-leipzig.de>
2021-05-26 12:53:43 +0200

    Changed Databasehelper

--- Data/Scripts/Database.py
+++ Data/Scripts/Database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def createTables():
 
-    con = sqlite3.connect('../chessData.db')
+    con = sqlite3.connect('../Data/chessData.db')
 
     with con:
         con.execute("""
@@ -10,12 +10,31 @@ def createTables():
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 gameName VARCHAR(255),
                 url VARCHAR(255),
+                initialURL VARCHAR(255),
                 scrapped BOOLEAN,
                 opening VARCHAR(255),
-                players VARCHAR(255)
+                players VARCHAR(255),
+                result varchar(15),
+                time varchar(127)
             );
         """)
 
     con.close()
 
-createTables()
+def writeGameIntoDB(gameName = "", url = "", initialURL = '', scrapped = False, opening = "", players = "", result = "", time = ""):
+
+    con = sqlite3.connect('../Data/chessData.db')
+
+    c = con.cursor()
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='GAMES' ''')
+
+    if c.fetchone()[0] != 1:
+        createTables()
+
+    with con:
+
+        sql = "INSERT INTO GAMES (gameName, url, initialURL, scrapped, opening, players, result, time) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
+
+        con.execute(sql, (gameName, url, initialURL, scrapped, opening, players, result, time))
+
+    con.close()
