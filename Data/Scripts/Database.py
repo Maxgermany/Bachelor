c718314549f33dcab82a23ba3db12faf881d9d6c
Maximus Germer <mg26kapo@studserv.uni-leipzig.de>
2021-05-27 10:21:59 +0200

    Added function to get unscrapped urls

--- Data/Scripts/Database.py
+++ Data/Scripts/Database.py
@@ -38,3 +38,19 @@ def writeGameIntoDB(gameName = "", url = "", initialURL = '', scrapped = False, 
         con.execute(sql, (gameName, url, initialURL, scrapped, opening, players, result, time))
 
     con.close()
+
+def getUnscrappedURLS(amount = 10):
+    con = sqlite3.connect('../Data/chessData.db')
+
+    c = con.cursor()
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='GAMES' ''')
+
+    if c.fetchone()[0] != 1:
+        createTables()
+
+    with con:
+        sql = "SELECT url FROM GAMES WHERE scrapped = 0 LIMIT ?"
+        c.execute(sql, (amount,))
+        return c.fetchall()
+
+    con.close()
