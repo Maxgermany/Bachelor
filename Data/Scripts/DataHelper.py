4536fa1673878681baf100906b494167d32ed2fc
Maximus Germer <mg26kapo@studserv.uni-leipzig.de>
2021-06-04 17:42:02 +0200

    Reworked corpus creation and used language detection package

--- Data/Scripts/DataHelper.py
+++ Data/Scripts/DataHelper.py
@@ -1,8 +1,7 @@
-import re
 from os import walk
 import os
 import json
-import string
+from langdetect import detect
 
 def sortFilesInFolder():
     _, _, filenames = next(walk("../Gameknot/JSON/"))
@@ -11,51 +10,37 @@ def sortFilesInFolder():
         if filename[0] == " ":
             os.rename("../Gameknot/JSON/" + filename, "../Gameknot/JSON/space/" + filename)
         else:
-            if os.path.exists("../Gameknot/JSON/" + filename[0] + "/"):
-                os.rename("../Gameknot/JSON/" + filename, "../Gameknot/JSON/" + filename[0] + "/" + filename)
-            else:
+            if not os.path.exists("../Gameknot/JSON/" + filename[0] + "/"):
                 os.mkdir("../Gameknot/JSON/" + filename[0] + "/")
-                os.rename("../Gameknot/JSON/" + filename, "../Gameknot/JSON/" + filename[0] + "/" + filename)
+            os.rename("../Gameknot/JSON/" + filename, "../Gameknot/JSON/" + filename[0] + "/" + filename)
 
 
 def createCorpus():
 
-    amountComments = 0
-
     folders = os.listdir("../Gameknot/JSON/")
 
-    file = open("../Gameknot/corpus.txt", "w+", encoding="utf-8")
+    with open("../Gameknot/corpus.txt", "w+", encoding="utf-8") as file:
+        for folder in folders:
+            _, _, filenames = next(walk("../Gameknot/JSON/" + folder + "/"))
+            for filename in filenames:
+                with open("../Gameknot/JSON/" + folder + "/" + filename, "r", encoding="utf-8") as jsonFile:
+                    content = json.load(jsonFile)
+                    for move in content["moves"]:
+                        file.write((content["moves"][move] + " ").replace("\n", " "))
 
-    for folder in folders:
-        _, _, filenames = next(walk("../Gameknot/JSON/" + folder + "/"))
-        for filename in filenames:
-            jsonFile = open("../Gameknot/JSON/" + folder + "/" + filename, "r", encoding="utf-8")
-            content = json.load(jsonFile)
-            for move in content["moves"]:
-                amountComments += 1
-                file.write((content["moves"][move] + " ").replace("\n", " "))
-            file.write("\n")
-            jsonFile.close()
-
-    file.close()
-
-    print("Found " + str(amountComments) + " move-commment-pairs")
+                    file.write("\n")
 
 def preprocessCorpus():
 
     processingFunctions = ["lowerCase", "filterOtherLanguages"]
 
-    file = open("../Gameknot/corpus.txt", "r+", encoding="utf-8")
-
-    content = file.read()
-
-    file.close()
+    with open("../Gameknot/corpus.txt", "r+", encoding="utf-8") as file:
+        content = file.read()
 
     for function in processingFunctions:
         content = eval(function + "(content)")
-        file = open("../Gameknot/corpus" + function + ".txt", "w+", encoding="utf-8")
-        file.write(content)
-        file.close()
+        with open("../Gameknot/corpus" + function + ".txt", "w+", encoding="utf-8") as file:
+            file.write(content)
 
 def lowerCase(content):
     return content.lower()
@@ -65,19 +50,19 @@ def filterOtherLanguages(content):
 
     contentTemp = ""
 
-    file = open("../Gameknot/filteredLines.txt", "w+", encoding="utf-8")
-
     for line in lines:
-        if any(x not in string.printable for x in line):
-            file.write(line)
-            file.write("\n")
-            file.write("-------------------------------------")
-            file.write("\n")
-        else:
-            contentTemp += line
-            contentTemp += "\n"
-
-    file.close()
-
+        if len(line) > 10:
+            try:
+                lang = detect(line)
+                with open("../Gameknot/corpus" + lang.upper() + ".txt", "a+", encoding="utf-8") as file:
+                    file.write(line)
+                    file.write('\n')
+                if lang == 'en':
+                    contentTemp += line
+                    contentTemp += "\n"
+            except:
+                with open("../Gameknot/corpusExcept.txt", "a+", encoding="utf-8") as file:
+                    file.write(line)
+                    file.write("\n")
     return contentTemp
 
