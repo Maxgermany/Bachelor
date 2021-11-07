aeac111a2cf69d6e0306a22605bed632c3d170ce
Maximus Germer <mg26kapo@studserv.uni-leipzig.de>
2021-11-07 11:29:30 +0100

    Added function to get a Comment by Id

--- Data/Scripts/DatabaseHelper.py
+++ Data/Scripts/DatabaseHelper.py
@@ -257,3 +257,18 @@ def getAllDataPointsOfComment(commentId=1):
 
     con.close()
 
+def getCommentById(commentId=1):
+    con = sqlite3.connect('../Data/chessData.db')
+
+    c = con.cursor()
+    c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='GAMECOMMENTS' ''')
+
+    if c.fetchone()[0] != 1:
+        createTables()
+
+    with con:
+        sql = "SELECT * FROM GAMECOMMENTS WHERE Id = ?"
+        c.execute(sql, (commentId,))
+        return c.fetchone()
+
+    con.close()
